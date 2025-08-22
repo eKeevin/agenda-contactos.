0f943ff2e91200c14407640f49b7238853e6000c
Kevin Hernan Pineda <kevinhernanpineda@gmail.com>
2025-08-21 23:26:15 -0300

    fix: change base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+
+iimport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',  
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
